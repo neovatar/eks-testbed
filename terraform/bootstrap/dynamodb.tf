1d38b3cccba0ee261a912bc1a0ca25e1149f7478
Thomas Seliger <tom@neovatar.org>
2018-09-23 11:04:34 +0200

    Add terraform for eks

--- terraform/bootstrap/dynamodb.tf
+++ terraform/bootstrap/dynamodb.tf
@@ -1,6 +1,6 @@
 # dynamodb table for locking the state file
-resource "aws_dynamodb_table" "terraform-state-lock" {
-  name = "terraform-state-lock"
+resource "aws_dynamodb_table" "tfstate-lock" {
+  name = "tfstate-lock"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
---
+++ terraform/eks/variables.tf
@@ -0,0 +1,8 @@
+#
+# Variables Configuration
+#
+
+variable "cluster-name" {
+  default = "eks-test"
+  type    = "string"
+}
