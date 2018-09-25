0cb00347e055f7a6bdc90c93fd40b478fdf42c5f
Thomas Seliger <tom@neovatar.org>
2018-09-25 10:01:05 +0200

    Refactor kubectl config and k8s config map

--- terraform/eks/eks-cluster.tf
+++ terraform/eks/eks-cluster.tf
@@ -3,7 +3,8 @@
 #  * IAM Role to allow EKS service to manage other AWS services
 #  * EC2 Security Group to allow networking traffic with EKS cluster
 #  * EKS Cluster
-#
+#  * kubectl config
+#  * config map to map node iam to k8s RBAC
 
 resource "aws_iam_role" "eks-test-cluster" {
   name = "eks-test-cluster"
@@ -61,16 +62,6 @@ resource "aws_security_group_rule" "eks-test-cluster-ingress-node-https" {
   type                     = "ingress"
 }
 
-# resource "aws_security_group_rule" "eks-demo-cluster-ingress-workstation-https" {
-#   cidr_blocks       = ["${local.workstation-external-cidr}"]
-#   description       = "Allow workstation to communicate with the cluster API Server"
-#   from_port         = 443
-#   protocol          = "tcp"
-#   security_group_id = "${aws_security_group.demo-cluster.id}"
-#   to_port           = 443
-#   type              = "ingress"
-# }
-
 resource "aws_eks_cluster" "eks-test" {
   name     = "${var.cluster-name}"
   role_arn = "${aws_iam_role.eks-test-cluster.arn}"
@@ -84,4 +75,66 @@ resource "aws_eks_cluster" "eks-test" {
     "aws_iam_role_policy_attachment.eks-test-cluster-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.eks-test-cluster-AmazonEKSServicePolicy",
   ]
+}
+
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.eks-test-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+
+  kubeconfig = <<KUBECONFIG
+
+
+apiVersion: v1
+clusters:
+- cluster:
+    server: ${aws_eks_cluster.eks-test.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.eks-test.certificate_authority.0.data}
+  name: kubernetes
+contexts:
+- context:
+    cluster: kubernetes
+    user: aws
+  name: aws
+current-context: aws
+kind: Config
+preferences: {}
+users:
+- name: aws
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1alpha1
+      command: aws-iam-authenticator
+      args:
+        - "token"
+        - "-i"
+        - "${var.cluster-name}"
+      env:
+        - name: AWS_PROFILE
+          value: "seligerit"
+KUBECONFIG
+}
+
+
+resource "local_file" "config-map-aws-auth" {
+    content     = "${local.config_map_aws_auth}"
+    filename = "yaml/config-map-aws-auth.yaml"
+    depends_on = ["local_file.kubectl-config"]
+
+  provisioner "local-exec" {
+    command = "kubectl --kubeconfig=yaml/kubectl-config.yaml apply -f yaml/config-map-aws-auth.yaml"
+  }
 }
--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -2,57 +2,6 @@
 # Outputs
 #
 
-locals {
-  config_map_aws_auth = <<CONFIGMAPAWSAUTH
-
-
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${aws_iam_role.eks-test-node.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-CONFIGMAPAWSAUTH
-
-  kubeconfig = <<KUBECONFIG
-
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.eks-test.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.eks-test.certificate_authority.0.data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster-name}"
-      env:
-        - name: AWS_PROFILE
-          value: "seligerit"
-KUBECONFIG
-}
-
 output "config_map_aws_auth" {
   value = "${local.config_map_aws_auth}"
 }
@@ -64,14 +13,4 @@ output "kubeconfig" {
 resource "local_file" "kubectl-config" {
     content     = "${local.kubeconfig}"
     filename = "yaml/kubectl-config.yaml"
-}
-
-resource "local_file" "config-map-aws-auth" {
-    content     = "${local.config_map_aws_auth}"
-    filename = "yaml/config-map-aws-auth.yaml"
-    depends_on = ["local_file.kubectl-config"]
-
-  provisioner "local-exec" {
-    command = "kubectl --kubeconfig=yaml/kubectl-config.yaml apply -f yaml/config-map-aws-auth.yaml"
-  }
 }
